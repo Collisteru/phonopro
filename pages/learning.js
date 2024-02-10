3517120858d3f166f810295f6565311916b51bd0
Sean Carter <seca6364@colorado.edu>
2024-02-09 22:22:07 -0700

    Main menu done

---
+++ pages/learning.js
@@ -0,0 +1,17 @@
+// pages/learning.js
+
+import Head from "next/head";
+
+const LearningPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Learning Mode</title>
+      </Head>
+      <h1>Learning Mode</h1>
+      <p>This is the Learning Mode page. Add your content here.</p>
+    </div>
+  );
+};
+
+export default LearningPage;
