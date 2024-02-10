28de61070c4e25ef28955008f5a4b417ad73ce32
literaryverses <98240549+literaryverses@users.noreply.github.com>
2024-02-10 00:40:15 -0700

    updating constants with ipa dataset

---
+++ constants.js
@@ -0,0 +1,97 @@
+export const IPA = {
+  "r\\": {
+    symbol: "ɹ",
+    name: "voiced alveolar approximant",
+    audiopath: "/audio/Alveolar_approximant.ogg.mp3",
+  },
+  l: {
+    symbol: "l",
+    name: "voiced alveolar lateral approximant",
+    audiopath: "/audio/Alveolar_lateral_approximant.ogg.mp3",
+  },
+  n: {
+    symbol: "n",
+    name: "voiced alveolar nasal",
+    audiopath: "/audio/Alveolar_nasal.ogg.mp3",
+  },
+  4: {
+    symbol: "ɾ",
+    name: "voiced alveolar tap",
+    audiopath: "/audio/Alveolar_tap.ogg.mp3",
+  },
+  r: {
+    symbol: "r",
+    name: "voiced alveolar trill",
+    audiopath: "/audio/Alveolar_trill.ogg.mp3",
+  },
+  m: {
+    symbol: "m",
+    name: "voiced bilabial nasal",
+    audiopath: "/audio/Bilabial_nasal.ogg.mp3",
+  },
+  "B\\": {
+    symbol: "ʙ",
+    name: "voiced bilabial trill",
+    audiopath: "/audio/Bilabial_trill.ogg.mp3",
+  },
+  u: {
+    symbol: "u",
+    name: "close back rounded vowel",
+    audiopath: "/audio/Close_back_rounded_vowel.ogg.mp3",
+  },
+  M: {
+    symbol: "ɯ",
+    name: "close back unrounded vowel",
+    audiopath: "/audio/Close_back_unrounded_vowel.ogg.mp3",
+  },
+  "}": {
+    symbol: "ʉ",
+    name: "close central rounded vowel",
+    audiopath: "/audio/Close_central_rounded_vowel.ogg.mp3",
+  },
+  1: {
+    symbol: "ɨ",
+    name: "close central unrounded vowel",
+    audiopath: "/audio/Close_central_unrounded_vowel.ogg.mp3",
+  },
+  y: {
+    symbol: "y",
+    name: "close front rounded vowel",
+    audiopath: "/audio/Close_front_rounded_vowel.ogg.mp3",
+  },
+  i: {
+    symbol: "i",
+    name: "close front unrounded vowel",
+    audiopath: "/audio/Close_front_unrounded_vowel.ogg.mp3",
+  },
+  o: {
+    symbol: "o",
+    name: "close-mid back rounded vowel",
+    audiopath: "/audio/Close-mid_back_rounded_vowel.ogg.mp3",
+  },
+  7: {
+    symbol: "ɤ",
+    name: "close-mid back unrounded vowel",
+    audiopath: "/audio/Close-mid_back_unrounded_vowel.ogg.mp3",
+  },
+  8: {
+    symbol: "ɵ",
+    name: "close-mid central rounded vowel",
+    audiopath: "/audio/Close-mid_central_rounded_vowel.ogg.mp3",
+  },
+  "@\\": {
+    symbol: "ɘ",
+    name: "close-mid central unrounded vowel",
+    audiopath: "/audio/Close-mid_central_unrounded_vowel.ogg.mp3",
+  },
+  2: {
+    symbol: "ø",
+    name: "close-mid front rounded vowel",
+    audiopath: "/audio/Close-mid_front_rounded_vowel.ogg.mp3",
+  },
+  e: {
+    symbol: "e",
+    name: "close-mid front unrounded vowel",
+    audiopath: "/audio/Close-mid_front_unrounded_vowel.ogg.mp3",
+  },
+};
