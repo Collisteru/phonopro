31cd8a5c9c43c0c96712c78bee912fae9bb04dd3
Sean Carter <seca6364@colorado.edu>
2024-02-09 22:06:47 -0700

    Added Tailwind CSS and buttons

--- components/TitlePage.js
+++ components/TitlePage.js
@@ -1,21 +1,36 @@
 // components/TitlePage.js
 
+import Button from "../components/Button";
+
 const TitlePage = () => {
+  const handleClick = () => {
+    // Add your logic here
+    console.log("Button clicked");
+  };
+
   return (
     <div>
       <div style={{ textAlign: "center", fontSize: "1.5rem" }}>
         <h1>Welcome to</h1>
-        <img
-          src="/fonopro_logo_close_cropped.png"
-          alt="Fonopro Logo"
-          style={{ width: "1000px", height: "auto" }}
-        />
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <img
+            src="/fonopro_logo_close_cropped.png"
+            alt="Fonopro Logo"
+            style={{ width: "1000px", height: "auto" }}
+          />
+        </div>
       </div>
-      <div style={{ textAlign: "center", fontSize: "2rem" }}>
+      <div
+        style={{ textAlign: "center", fontSize: "2rem", marginBottom: "5rem" }}
+      >
         <p>
           An Educational Project for Blasterhacks 2024 By{" "}
           <strong>Jun Lee and Sean Carter</strong>
         </p>
+      </div>
+      <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+        <Button onClick={handleClick}>Learning Mode</Button>
+        <Button onClick={handleClick}>Quizzing Mode</Button>
       </div>
     </div>
   );
