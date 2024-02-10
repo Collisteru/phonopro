90f3a0f1d505f463c03c67ce5e810b8f72031ece
Sean Carter <seca6364@colorado.edu>
2024-02-10 12:15:59 -0700

    Working on multipage quizzes

--- pages/quizzing.js
+++ pages/quizzing.js
@@ -5,6 +5,10 @@ import Footer from "@components/Footer";
 import Button from "@components/Button";
 import Link from "next/link";
 
+const [correctCount, setCorrectCount] = useState(0);
+const [quizIndex, setQuizIndex] = useState(0);
+const totalQuizzes = 5; // Change this to the total number of quizzes
+
 const QuizzingPage = () => {
   return (
     <div>
@@ -30,4 +34,34 @@ const QuizzingPage = () => {
   );
 };
 
+const handleAnswer = (isCorrect) => {
+  if (isCorrect) {
+    setCorrectCount((prevCount) => prevCount + 1);
+  }
+  setQuizIndex((prevIndex) => prevIndex + 1);
+};
+
+const resetQuiz = () => {
+  setCorrectCount(0);
+  setQuizIndex(0);
+};
+
+return (
+  <div>
+    <h1>Quiz App</h1>
+    {quizIndex < totalQuizzes ? (
+      <QuizPage
+        key={quizIndex} // Ensure each quiz instance gets a unique key
+        onAnswer={handleAnswer}
+        onNext={() => setQuizIndex((prevIndex) => prevIndex + 1)}
+      />
+    ) : (
+      <div>
+        <h2>Quiz Complete!</h2>
+        <p>Correct Answers: {correctCount}</p>
+        <button onClick={resetQuiz}>Restart Quiz</button>
+      </div>
+    )}
+  </div>
+);
 export default QuizzingPage;
