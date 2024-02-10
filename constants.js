2b15e6910274648958c25adece3e37457ba6422a
literaryverses <98240549+literaryverses@users.noreply.github.com>
2024-02-10 01:24:20 -0700

    constants update

--- constants.js
+++ constants.js
@@ -1,97 +1,247 @@
 export const IPA = {
-  "r\\": {
+  151: {
     symbol: "ɹ",
     name: "voiced alveolar approximant",
     audiopath: "/audio/Alveolar_approximant.ogg.mp3",
   },
-  l: {
+  155: {
     symbol: "l",
     name: "voiced alveolar lateral approximant",
     audiopath: "/audio/Alveolar_lateral_approximant.ogg.mp3",
   },
-  n: {
+  116: {
     symbol: "n",
     name: "voiced alveolar nasal",
     audiopath: "/audio/Alveolar_nasal.ogg.mp3",
   },
-  4: {
+  124: {
     symbol: "ɾ",
     name: "voiced alveolar tap",
     audiopath: "/audio/Alveolar_tap.ogg.mp3",
   },
-  r: {
+  122: {
     symbol: "r",
     name: "voiced alveolar trill",
     audiopath: "/audio/Alveolar_trill.ogg.mp3",
   },
-  m: {
+  114: {
     symbol: "m",
     name: "voiced bilabial nasal",
     audiopath: "/audio/Bilabial_nasal.ogg.mp3",
   },
-  "B\\": {
+  121: {
     symbol: "ʙ",
     name: "voiced bilabial trill",
     audiopath: "/audio/Bilabial_trill.ogg.mp3",
   },
-  u: {
+  308: {
     symbol: "u",
     name: "close back rounded vowel",
     audiopath: "/audio/Close_back_rounded_vowel.ogg.mp3",
   },
-  M: {
+  316: {
     symbol: "ɯ",
     name: "close back unrounded vowel",
     audiopath: "/audio/Close_back_unrounded_vowel.ogg.mp3",
   },
-  "}": {
+  318: {
     symbol: "ʉ",
     name: "close central rounded vowel",
     audiopath: "/audio/Close_central_rounded_vowel.ogg.mp3",
   },
-  1: {
+  317: {
     symbol: "ɨ",
     name: "close central unrounded vowel",
     audiopath: "/audio/Close_central_unrounded_vowel.ogg.mp3",
   },
-  y: {
+  309: {
     symbol: "y",
     name: "close front rounded vowel",
     audiopath: "/audio/Close_front_rounded_vowel.ogg.mp3",
   },
-  i: {
+  301: {
     symbol: "i",
     name: "close front unrounded vowel",
     audiopath: "/audio/Close_front_unrounded_vowel.ogg.mp3",
   },
-  o: {
+  307: {
     symbol: "o",
     name: "close-mid back rounded vowel",
     audiopath: "/audio/Close-mid_back_rounded_vowel.ogg.mp3",
   },
-  7: {
+  315: {
     symbol: "ɤ",
     name: "close-mid back unrounded vowel",
     audiopath: "/audio/Close-mid_back_unrounded_vowel.ogg.mp3",
   },
-  8: {
+  323: {
     symbol: "ɵ",
     name: "close-mid central rounded vowel",
     audiopath: "/audio/Close-mid_central_rounded_vowel.ogg.mp3",
   },
-  "@\\": {
+  397: {
     symbol: "ɘ",
     name: "close-mid central unrounded vowel",
     audiopath: "/audio/Close-mid_central_unrounded_vowel.ogg.mp3",
   },
-  2: {
+  310: {
     symbol: "ø",
     name: "close-mid front rounded vowel",
     audiopath: "/audio/Close-mid_front_rounded_vowel.ogg.mp3",
   },
-  e: {
+  302: {
     symbol: "e",
     name: "close-mid front unrounded vowel",
     audiopath: "/audio/Close-mid_front_unrounded_vowel.ogg.mp3",
+  },
+  113: {
+    symbol: "ʔ",
+    name: "glottal stop",
+    audiopath: "/audio/Glottal_stop.ogg.mp3",
+  },
+  150: {
+    symbol: "ʋ",
+    name: "voiced labiodental approximant",
+    audiopath: "/audio/Labiodental_approximant.mp3",
+  },
+  115: {
+    symbol: "ɱ",
+    name: "voiced labiodental nasal",
+    audiopath: "/audio/Labiodental_nasal.ogg.mp3",
+  },
+  322: {
+    symbol: "ə",
+    name: "mid central vowel",
+    audiopath: "/audio/Mid_central_vowel.ogg.mp3",
+  },
+  321: {
+    symbol: "ʊ",
+    name: "near-close near-back rounded vowel",
+    audiopath: "/audio/Near-close_near-back_rounded_vowel.ogg.mp3",
+  },
+  320: {
+    symbol: "Y",
+    name: "near-close near-front rounded vowel",
+    audiopath: "/audio/Near-close_near-front_rounded_vowel.ogg.mp3",
+  },
+  319: {
+    symbol: "ɪ",
+    name: "near-close near-front unrounded vowel",
+    audiopath: "/audio/Near-close_near-front_unrounded_vowel.ogg.mp3",
+  },
+  324: {
+    symbol: "ɐ",
+    name: "near-open central vowel",
+    audiopath: "/audio/Near-open_central_unrounded_vowel.ogg.mp3",
+  },
+  325: {
+    symbol: "æ",
+    name: "near-open front unrounded vowel",
+    audiopath: "/audio/Near-open_front_unrounded_vowel.ogg.mp3",
+  },
+  313: {
+    symbol: "ɒ",
+    name: "open back rounded vowel",
+    audiopath: "/audio/Open-back_rounded_vowel.ogg.mp3",
+  },
+  305: {
+    symbol: "ɑ",
+    name: "open back unrounded vowel",
+    audiopath: "/audio/Open_back_unrounded_vowel.ogg.mp3",
+  },
+  312: {
+    symbol: "ɶ",
+    name: "open front rounded vowel",
+    audiopath: "/audio/Open_front_rounded_vowel.ogg.mp3",
+  },
+  304: {
+    symbol: "a",
+    name: "open front unrounded vowel",
+    audiopath: "/audio/Open_front_unrounded_vowel.ogg.mp3",
+  },
+  306: {
+    symbol: "ɔ",
+    name: "open-mid back rounded vowel",
+    audiopath: "/audio/Open-mid_back_rounded_vowel.ogg.mp3",
+  },
+  314: {
+    symbol: "ʌ",
+    name: "open-mid back unrounded vowel",
+    audiopath: "/audio/Open-mid_back_unrounded_vowel.ogg.mp3",
+  },
+  395: {
+    symbol: "ɞ",
+    name: "open-mid central rounded vowel",
+    audiopath: "/audio/Open-mid_central_rounded_vowel.ogg.mp3",
+  },
+  326: {
+    symbol: "ɜ",
+    name: "open-mid central unrounded vowel",
+    audiopath: "/audio/Open-mid_central_unrounded_vowel.ogg.mp3",
+  },
+  311: {
+    symbol: "œ",
+    name: "open-mid front rounded vowel",
+    audiopath: "/audio/Open-mid_front_rounded_vowel.ogg.mp3",
+  },
+  303: {
+    symbol: "ɛ",
+    name: "open-mid front unrounded vowel",
+    audiopath: "/audio/Open-mid_front_unrounded_vowel.ogg.mp3",
+  },
+  153: {
+    symbol: "j",
+    name: "voiced palatal approximant",
+    audiopath: "/audio/Palatal_approximant.ogg.mp3",
+  },
+  157: {
+    symbol: "ʎ",
+    name: "voiced palatal lateral approximant",
+    audiopath: "/audio/Palatal_lateral_approximant.ogg.mp3",
+  },
+  118: {
+    symbol: "ɲ",
+    name: "voiced palatal nasal",
+    audiopath: "/audio/Palatal_nasal.ogg.mp3",
+  },
+  152: {
+    symbol: "ɻ",
+    name: "voiced retroflex approximant",
+    audiopath: "/audio/Retroflex_approximant.ogg.mp3",
+  },
+  125: {
+    symbol: "ɽ",
+    name: "voiced retroflex flap",
+    audiopath: "/audio/Retroflex_flap.ogg.mp3",
+  },
+  156: {
+    symbol: "ɭ",
+    name: "voiced retroflex lateral approximant",
+    audiopath: "/audio/Retroflex_lateral_approximant.ogg.mp3",
+  },
+  117: {
+    symbol: "ɳ",
+    name: "voiced retroflex nasal",
+    audiopath: "/audio/Retroflex_nasal.ogg.mp3",
+  },
+  120: {
+    symbol: "ɴ",
+    name: "voiced uvular nasal",
+    audiopath: "/audio/Uvular_nasal.ogg.mp3",
+  },
+  123: {
+    symbol: "ʀ",
+    name: "voiced uvular trill",
+    audiopath: "/audio/Uvular_trill.ogg.mp3",
+  },
+  158: {
+    symbol: "ʟ",
+    name: "voiced velar lateral approximant",
+    audiopath: "/audio/Velar_lateral_approximant.ogg.mp3",
+  },
+  119: {
+    symbol: "ŋ",
+    name: "voiced velar nasal",
+    audiopath: "/audio/Velar_nasal.ogg.mp3",
   },
 };
