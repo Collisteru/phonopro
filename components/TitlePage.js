4230b4f254c6144238afec92601ff71d56e4f07a
Sean Carter <seca6364@colorado.edu>
2024-02-09 21:26:02 -0700

    Added TitlePage, started working on the CSS file

---
+++ components/TitlePage.js
@@ -0,0 +1,18 @@
+// components/TitlePage.js
+
+const TitlePage = () => {
+  return (
+    <div style={{ textAlign: "center", fontSize: "3rem" }}>
+      <div style={{ textAlign: "center", fontSize: "3rem" }}>
+        <h1>Welcome to</h1>
+        <h1>PhonoPro</h1>
+      </div>
+      <div style={{ textAlign: "center", fontSize: "2rem" }}>
+        <p>An Eductational Project for Blasterhacks 2024</p>
+        <p>By Jun Lee and Sean Carter</p>
+      </div>
+    </div>
+  );
+};
+
+export default TitlePage;
