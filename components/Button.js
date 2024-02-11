d72c1b3690c0fd6136874b2141301c26a66fbf31
Sean Carter <seca6364@colorado.edu>
2024-02-11 00:21:48 -0700

    Fixed variable button height in main menu on mobile

--- components/Button.js
+++ components/Button.js
@@ -3,7 +3,7 @@
 const Button = ({ children, onClick, className }) => {
   return (
     <button
-      className={`border border-black bg-white text-black font-bold py-2 px-4 ${className}`}
+      className={`min-h-[80px] border border-black bg-white text-black font-bold py-2 px-4 ${className}`}
       onClick={onClick}
     >
       {children}
